28e6adc757a9bb914879dc872acb56a8e672e514
Rich Harris <rich.harris@vercel.com>
2024-10-10 12:41:37 -0400

    target main branch

--- apps/svelte.dev/scripts/sync-docs/index.ts
+++ apps/svelte.dev/scripts/sync-docs/index.ts
@@ -27,7 +27,7 @@ const packages: Package[] = [
 		name: 'svelte',
 		local: `${REPOS}/svelte`,
 		repo: 'sveltejs/svelte',
-		branch: 'docs-fixes',
+		branch: 'main',
 		pkg: 'packages/svelte',
 		docs: 'documentation/docs',
 		process_modules: async (modules: Modules) => {
