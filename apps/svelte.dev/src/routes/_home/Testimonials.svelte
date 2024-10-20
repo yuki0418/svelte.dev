df96995f3e8bf551c63c293711aed4819dcba25d
Braden Wiggins <55504972+braebo@users.noreply.github.com>
2024-10-19 20:57:56 -0400

    fix: testemonial images on safari (#475)
    * fix: testemonial images on safari
    
    - changed `filter: drop-shadow` to `box-shadow`
    - added a subtly larger shadow to the hover transition
    - moved the shadow from the `a` to the `img` and removed `overflow: hidden` from `a`
    - changed `filter: none` to `filter: grayscale(0)` to fix the transition on safari
    
    `overflow: hidden` caused safari to clip the shadows regardless of whether they were defined on the `a` or `img` tag for both `drop` and `box` shadows.
    
    * fix: back to drop-shadow filter
    
    Turns out `transfrom: translate3d(0,0,0)` was already there, but moving it from the `img` to the `a` tag did the trick!
    
    * turn it down just a smidge
    
    ---------
    
    Co-authored-by: Rich Harris <rich.harris@vercel.com>

--- apps/svelte.dev/src/routes/_home/Testimonials.svelte
+++ apps/svelte.dev/src/routes/_home/Testimonials.svelte
@@ -72,10 +72,11 @@
 			display: block;
 			width: 100%;
 			top: 0;
-			filter: drop-shadow(0.2rem 0.4rem 2rem rgb(0 0 0 / 0.3));
 			overflow: hidden;
-			transition: 0.2s;
-			transition-property: scale, filter;
+			filter: drop-shadow(0.2rem 0.4rem 1.6rem rgb(0 0 0 / 0.2));
+			transform: translate3d(0, 0, 0);
+			transition-property: filter, scale;
+			transition-duration: 0.2s;
 
 			&:nth-child(1) {
 				top: 0%;
@@ -99,6 +100,7 @@
 
 			&:hover {
 				scale: 1.05;
+				filter: drop-shadow(0.2rem 0.4rem 2.4rem rgb(0 0 0 / 0.2));
 			}
 		}
 
@@ -108,9 +110,10 @@
 			object-fit: contain;
 			border-radius: var(--sk-border-radius);
 			filter: grayscale(1);
-			-webkit-transform: translate3d(0, 0, 0);
+			transition: filter 0.2s;
+
 			a:hover & {
-				filter: none;
+				filter: grayscale(0);
 			}
 		}
 	}
