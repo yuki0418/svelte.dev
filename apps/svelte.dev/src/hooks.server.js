59fbe4719cfac7acc9de72295fa38e5454fc4cf1
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-10-23 15:37:52 -0700

    fix: add more svelte.dev redirects (#595)

--- apps/svelte.dev/src/hooks.server.js
+++ apps/svelte.dev/src/hooks.server.js
@@ -2,12 +2,35 @@ import { redirect } from '@sveltejs/kit';
 
 const mappings = new Map([
 	['/docs/accessibility-warnings', '/docs/svelte/compiler-warnings'],
+	['/docs/basic-markup', '/docs/svelte/basic-markup'],
+	['/docs/client-side-component-api', '/docs/svelte/legacy-component-api'],
+	// no good mapping for this one
 	['/docs/component-directives', '/docs/svelte/svelte-files'],
 	['/docs/custom-elements-api', '/docs/svelte/custom-elements'],
+	// no good mapping for this one
 	['/docs/element-directives', '/docs/svelte/basic-markup'],
+	['/docs/introduction', '/docs/svelte/overview'],
+	// no good mapping for this one
 	['/docs/logic-blocks', '/docs/svelte/basic-markup'],
-	['/docs/svelte-components', '/docs/svelte/svelte-files'],
+	['/docs/server-side-component-api', '/docs/svelte/legacy-component-api'],
+	// no good mapping for this one
+	['/docs/special-elements', '/docs/svelte/svelte-window'],
 	['/docs/special-tags', '/docs/svelte/basic-markup'],
+	['/docs/svelte', '/docs/svelte/svelte'],
+	['/docs/svelte-action', '/docs/svelte/svelte-action'],
+	['/docs/svelte-animate', '/docs/svelte/svelte-animate'],
+	['/docs/svelte-compiler', '/docs/svelte/svelte-compiler'],
+	['/docs/svelte-components', '/docs/svelte/svelte-files'],
+	['/docs/svelte-easing', '/docs/svelte/svelte-easing'],
+	['/docs/svelte-motion', '/docs/svelte/svelte-motion'],
+	[
+		'/docs/svelte-register',
+		'https://github.com/sveltejs/svelte/blob/svelte-4/documentation/docs/06-legacy/01-svelte-register.md'
+	],
+	['/docs/svelte-store', '/docs/svelte/svelte-store'],
+	['/docs/svelte-transition', '/docs/svelte/svelte-transition'],
+	['/docs/typescript', '/docs/svelte/typescript'],
+	['/docs/v4-migration-guide', '/docs/svelte/v4-migration-guide'],
 	['/faq', '/docs/svelte/faq']
 ]);
 
