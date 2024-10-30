b917545d7413d171e9e715ffa65114b883590641
Rich Harris <rich.harris@vercel.com>
2024-10-30 15:19:04 -0400

    set inlineStyleThreshold (#724)

--- apps/svelte.dev/svelte.config.js
+++ apps/svelte.dev/svelte.config.js
@@ -7,6 +7,9 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+
+		inlineStyleThreshold: 1000,
+
 		prerender: {
 			handleMissingId(warning) {
 				if (warning.id.startsWith('H4sIA')) {
