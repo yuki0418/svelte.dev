425b7b8094870830d91c5428bd3793c044555705
Rich Harris <rich.harris@vercel.com>
2025-03-14 15:50:01 -0400

    fix: remove all backticks from titles (#1189)

--- packages/site-kit/src/lib/server/content/index.ts
+++ packages/site-kit/src/lib/server/content/index.ts
@@ -34,7 +34,7 @@ export async function create_index(
 		const sections = Array.from(body.matchAll(/^##\s+(.*)$/gm)).map((match) => {
 			const title = smart_quotes(match[1])
 				// replace < and > inside code spans
-				.replace(/`(.+?)`/, (_, contents) => contents.replace(/</g, '&lt;').replace(/>/g, '&gt;'))
+				.replace(/`(.+?)`/g, (_, contents) => contents.replace(/</g, '&lt;').replace(/>/g, '&gt;'))
 				// turn e.g. `class:_name_` into `class:<em>name</em>`
 				.replace(/_(.+)_/g, (_, contents) => `<em>${contents}</em>`);
 
