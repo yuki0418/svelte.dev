a324f577fa36bc5fc8796383bbb674947649f088
Rich Harris <rich.harris@vercel.com>
2025-03-18 21:05:40 -0400

    fix: convert type assertions (#1204)
    * fix: convert type assertions
    
    * fix

--- packages/site-kit/src/lib/markdown/renderer.ts
+++ packages/site-kit/src/lib/markdown/renderer.ts
@@ -523,8 +523,20 @@ async function convert_to_ts(js_code: string, indent = '', offset = '') {
 					while (code.original[start - 1] !== ')') start -= 1;
 					code.appendLeft(start, `: ${returns}`);
 				}
+			} else if (type && ts.isParenthesizedExpression(node)) {
+				// convert `/* @type {Foo} */ (foo)` to `foo as Foo`
+				// TODO one day we may need to account for operator precedence
+				// (i.e. preserve the parens in e.g. `(x as y).z()`)
+				let start = node.getStart();
+				while (js_code[start - 1] !== '/') start -= 1;
+				code.remove(start, node.getStart() + 1);
+
+				let end = node.getEnd();
+				code.overwrite(end - 1, end, ` as ${type}`);
 			} else {
-				throw new Error('Unhandled @type JsDoc->TS conversion: ' + js_code);
+				throw new Error(
+					'Unhandled @type JsDoc->TS conversion: ' + js_code.slice(node.getStart(), node.getEnd())
+				);
 			}
 
 			if (!comment) {
@@ -532,7 +544,9 @@ async function convert_to_ts(js_code: string, indent = '', offset = '') {
 				let start = jsdoc[0].getStart();
 				let end = jsdoc[0].getEnd();
 
-				while (start > 0 && code.original[start] !== '\n') start -= 1;
+				while (start > 0 && code.original[start - 1] === '\t') start -= 1;
+				while (start > 0 && code.original[start - 1] === '\n') start -= 1;
+
 				code.overwrite(start, end, '');
 			}
 		}
@@ -560,7 +574,7 @@ async function convert_to_ts(js_code: string, indent = '', offset = '') {
 			while (js_code[i] !== '\n') i += 1;
 			i += 1;
 
-			code.appendLeft(i, import_statements + '\n');
+			code.appendLeft(i, '\n' + import_statements + '\n');
 		} else {
 			code.prependLeft(0, offset + import_statements + '\n');
 		}
