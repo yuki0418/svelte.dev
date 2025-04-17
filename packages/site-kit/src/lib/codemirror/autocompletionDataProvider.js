777de784832b68844dae4002ebcfec80cc32d7d2
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2025-04-16 23:33:50 -0700

    fix: fix autocomplete for `$props.id()` (#1307)
    * update autocomplete stuff to current svelte 5
    
    * update autocomplete
    
    * lint
    
    * fix autocomplete for `$props.id()`
    
    * lint
    
    * add file type check
    
    * fix file check, the types were a lie

--- packages/site-kit/src/lib/codemirror/autocompletionDataProvider.js
+++ packages/site-kit/src/lib/codemirror/autocompletionDataProvider.js
@@ -576,11 +576,20 @@ const is_bindable = (node, context) => {
 /**
  * @type {import("./types").Test}
  */
-const is_props_id = (node) => {
-	if (node.parent?.name !== 'VariableDeclaration') return false;
-	if (node.parent?.parent?.name !== 'VariableDeclarator') return false;
-	if (node.parent?.parent?.parent?.name !== 'Program') return false;
-	if (node.parent?.firstChild?.name !== 'Identifier') return false;
+const is_props_id_call = (node, context, selected) => {
+	if (!selected.match(/\.svelte$/)) return false;
+	if (!is_state_call(node, context, selected)) return false;
+	if (node.parent?.parent?.name !== 'Script') return false;
+	return true;
+};
+
+/**
+ * @type {import("./types").Test}
+ */
+const is_props_id = (node, context, selected) => {
+	if (!selected.match(/\.svelte$/)) return false;
+	if (!is_state(node, context, selected)) return false;
+	if (node.parent?.parent?.name !== 'Script') return false;
 	return true;
 };
 
@@ -588,6 +597,7 @@ export const runes = [
 	{ snippet: '$state(${})', test: is_state },
 	{ snippet: '$state', test: is_state_call },
 	{ snippet: '$props()', test: is_props },
+	{ snippet: '$props.id', test: is_props_id_call },
 	{ snippet: '$props.id()', test: is_props_id },
 	{ snippet: '$derived(${});', test: is_state },
 	{ snippet: '$derived', test: is_state_call },
