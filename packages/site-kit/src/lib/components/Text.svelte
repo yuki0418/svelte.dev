9956e562430c0a089ce49a9864b86eab37cffb56
Wai Yan Min Lwin <waiyanminlwin421@gmail.com>
2025-07-08 11:04:49 +0700

    fix: copy the correct code block (#1406)
    * bug: Fix copy button copying wrong code for Issue #1403
    
    * fix: copy the correct code block
    
    * format
    
    ---------
    
    Co-authored-by: Chew Tee Ming <chew.tee.ming@nindatech.com>

--- packages/site-kit/src/lib/components/Text.svelte
+++ packages/site-kit/src/lib/components/Text.svelte
@@ -33,10 +33,8 @@
 				.find((node) => (node as HTMLElement).classList.contains('code-block')) as HTMLElement;
 
 			const ts = !!parent.querySelector('.ts-toggle:checked');
-			const code = parent.querySelector(
-				`pre[data-language]:${ts ? 'last' : 'first'}-of-type code`
-			) as HTMLElement;
-
+			const query = ts ? `pre[data-language="ts"] code` : 'pre code';
+			const code = parent.querySelector(query) as HTMLElement;
 			navigator.clipboard.writeText(get_text(code));
 		}
 	}
